b53bc6c67ee18c0773eac4300e596435dcfbe78e
Jonathan Ross <jonross@alum.mit.edu>
2024-02-25 12:23:20 -0500

    Add jmespath performance test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,1 +1,25 @@
-import pytest
+import time
+
+import jmespath
+
+
+def test_jmespath_performance():
+    """
+    JMESPath performance regression test.  We use JMESPath to filter and transform
+    the data returned by the Kubernetes API.
+    """
+    path = jmespath.compile("pods[?status.phase == 'Running'].metadata.name")
+    data = {
+        "pods": [
+            {"status": {"phase": "Running"}, "metadata": {"name": "pod-1"}},
+            {"status": {"phase": "Running"}, "metadata": {"name": "pod-2"}},
+            {"status": {"phase": "Pending"}, "metadata": {"name": "pod-3"}},
+        ]
+    }
+    start = time.time()
+    for _ in range(10000):
+        result = path.search({**data})
+    end = time.time()
+    assert end - start < 1.0
+    assert result == ["pod-1", "pod-2"]
+
