5a2e346c0a941aae10a556eb92079f9e8900720b
Jonathan Ross <jonross@alum.mit.edu>
2024-11-29 21:59:41 -0500

    Remove dbmodel.py, no longer needed

--- lib/kubeql/tables.py
+++ lib/kubeql/tables.py
@@ -1,6 +1,33 @@
-
-from .dbmodel import Table
+from .config import Config, EMPTY_EXTENSION, ColumnDef
 from .helpers import Resources, ItemHelper, PodHelper, JobHelper
+
+
+class Table:
+    """
+    Turn 'kubectl get ... -o json" output into a database table.  Subclasses define
+        NAME        the table name
+        SCHEMA      chunk of DDL with column names and types
+        make_rows   method to convert kubectl output into rows
+    """
+
+    def create(self, db, config: Config, kube_data: dict):
+        schema = self.SCHEMA
+        extra_columns = config.extend.get(self.NAME, EMPTY_EXTENSION).columns
+        if extra_columns:
+            schema += " ".join(f", {name} {column._sqltype}"
+                               for name, column in extra_columns.items())
+        db.execute(f"CREATE TABLE {self.NAME} ({schema})")
+        rows = self.make_rows(kube_data["items"])
+        if rows:
+            if extra_columns:
+                rows = [row + tuple(self._convert(item, column) for column in extra_columns.values())
+                        for item, row in zip(kube_data["items"], rows)]
+            placeholders = ", ".join("?" * len(rows[0]))
+            db.execute(f"INSERT INTO {self.NAME} VALUES({placeholders})", rows)
+
+    def _convert(self, obj: object, column: ColumnDef) -> object:
+        value = column._finder(obj)
+        return None if value is None else column._pytype(value)
 
 
 class NodesTable(Table):
