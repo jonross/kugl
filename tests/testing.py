379f3c850593d35cc3bc6e457c0aa609c37e2c83
Jonathan Ross <jonross@alum.mit.edu>
2024-12-03 20:34:49 -0500

    Move sample job and pod markup from testing.py to resources/ folder

--- tests/testing.py
+++ tests/testing.py
@@ -45,7 +45,7 @@ def make_pod(name: str,
     :param mem_lim: Memory limit
     :param gpu: Number of GPUs requested / limit
     """
-    obj = yaml.safe_load(BASE_POD_YAML)
+    obj = yaml.safe_load(_resource("sample_pod.yaml"))
     if name_at_root:
         obj["name"] = name
     elif not no_name:
@@ -63,149 +63,6 @@ def make_pod(name: str,
     return obj
 
 
-BASE_POD_YAML = """
-    apiVersion: v1
-    kind: Pod
-    metadata:
-      annotations:
-        example.com/abc: an annotation
-        example.com/def: another annotation
-      creationTimestamp: '2024-02-04T15:15:01Z'
-      labels:
-        example.com/uvw: a label
-        example.com/xyz: another label
-        job-name: my-important-job
-      namespace: research
-      ownerReferences:
-      - apiVersion: batch/v1
-        blockOwnerDeletion: true
-        controller: true
-        kind: Job
-        name: my-important-job
-        uid: f1452695-3268-49ac-8018-a3d91a1d0082
-      resourceVersion: '2385628922'
-      uid: 4e1ba9fe-2483-4881-88c8-a5b14b9998c1
-    spec:
-      containers:
-      - command:
-        - /bin/bash
-        - -c
-        - "echo 'Hello world'"
-        env:
-        - name: MY_JOB_NAME
-          valueFrom:
-            fieldRef:
-              apiVersion: v1
-              fieldPath: metadata.labels['job-name']
-        - name: MY_POD_IP
-          valueFrom:
-            fieldRef:
-              apiVersion: v1
-              fieldPath: status.podIP
-        - name: SOME_VAR
-          value: SOME_VAL
-        envFrom:
-        - secretRef:
-            name: super-secret-secret
-            optional: true
-        image: example.com/hello-world
-        imagePullPolicy: Always
-        name: main
-        volumeMounts:
-        - mountPath: /utils
-          name: utils-volume
-          readOnly: true
-      dnsPolicy: ClusterFirst
-      enableServiceLinks: true
-      imagePullSecrets:
-      - name: i-can-haz-images
-      - name: i-can-haz-more-images
-      nodeName: worker5
-      preemptionPolicy: PreemptLowerPriority
-      priority: 7500
-      priorityClassName: important-stuff
-      restartPolicy: Never
-      schedulerName: my-scheduler
-      securityContext: {}
-      serviceAccount: default
-      serviceAccountName: default
-      terminationGracePeriodSeconds: 30
-      tolerations:
-      - key: example.com/research-node
-        operator: Equal
-        value: yes
-      - effect: NoExecute
-        key: node.kubernetes.io/not-ready
-        operator: Exists
-        tolerationSeconds: 300
-      - effect: NoExecute
-        key: node.kubernetes.io/unreachable
-        operator: Exists
-        tolerationSeconds: 300
-      volumes:
-      - hostPath:
-          path: /opt/pod-utils
-          type: Directory
-        name: utils-volume
-      - configMap:
-          defaultMode: 420
-          name: extra-utils
-        name: extra-utils
-      - emptyDir:
-          medium: Memory
-        name: dshm
-      - downwardAPI:
-          defaultMode: 420
-          items:
-          - fieldRef:
-              apiVersion: v1
-              fieldPath: metadata.labels
-            path: labels
-          - fieldRef:
-              apiVersion: v1
-              fieldPath: metadata.annotations
-            path: annotations
-        name: podinfo
-    status:
-      conditions:
-      - lastProbeTime: null
-        lastTransitionTime: '2024-02-04T15:15:01Z'
-        status: 'True'
-        type: Initialized
-      - lastProbeTime: null
-        lastTransitionTime: '2024-02-04T15:15:04Z'
-        status: 'True'
-        type: Ready
-      - lastProbeTime: null
-        lastTransitionTime: '2024-02-04T15:15:04Z'
-        status: 'True'
-        type: ContainersReady
-      - lastProbeTime: null
-        lastTransitionTime: '2024-02-04T15:15:01Z'
-        status: 'True'
-        type: PodScheduled
-      containerStatuses:
-      - containerID: docker://8627eb5f689cf5d0c9655a32e558066b0f2ddf566f2a93002b12a24f33b8cca0
-        image: docker.example.com/example-com/hello-world:8eb819ada5834aabb188cd6889abe0f48dba80ed
-        imageID: docker-pullable://docker.example.com/example-com/hello-world@sha256:cc6fc551890fd26d321b326eadf1c5dd249ea0034b6f0328f60593f245b9ed5b
-        lastState: {}
-        name: main
-        ready: true
-        restartCount: 0
-        started: true
-        state:
-          running:
-            startedAt: '2024-02-04T15:15:04Z'
-      hostIP: 10.11.12.13
-      phase: Running
-      podIP: 10.200.201.202
-      podIPs:
-      - ip: 10.200.201.202
-      qosClass: Burstable
-      startTime: '2024-02-04T15:15:01Z'
-"""
-
-
 def make_job(name: str,
              active_count: Optional[int] = None,
              condition: Optional[Tuple[str, str, Optional[str]]] = None,
@@ -218,7 +75,7 @@ def make_job(name: str,
     :param active_count: If present, the number of active pods
     :param condition: If present, a condition tuple (type, status, reason)
     """
-    obj = yaml.safe_load(BASE_JOB_YAML)
+    obj = yaml.safe_load(_resource("sample_job.yaml"))
     obj["metadata"]["name"] = name
     obj["metadata"]["labels"]["job-name"] = name
     if active_count is not None:
@@ -227,43 +84,6 @@ def make_job(name: str,
         obj["status"]["conditions"] = [{"type": condition[0], "status": condition[1], "reason": condition[2]}]
     return obj
 
-BASE_JOB_YAML = """
-    apiVersion: batch/v1
-    kind: Job
-    metadata:
-      creationTimestamp: "2024-11-20T01:05:00Z"
-      generation: 1
-      labels:
-        controller-uid: 60848f11-1ecb-4a20-b9aa-bc039cb98b88
-        job-name: example-job-1
-      name: example-job-1
-      namespace: example
-      resourceVersion: "3479929701"
-      uid: 60848f11-1ecb-4a20-b9aa-bc039cb98b88
-    spec:
-      backoffLimit: 0
-      completionMode: NonIndexed
-      completions: 1
-      parallelism: 1
-      selector:
-        matchLabels:
-          controller-uid: 60848f11-1ecb-4a20-b9aa-bc039cb98b88
-      suspend: false
-      template:
-        metadata:
-          creationTimestamp: null
-          labels:
-            controller-uid: 60848f11-1ecb-4a20-b9aa-bc039cb98b88
-            job-name: example-job-28867745
-        spec:
-          containers:
-          - command:
-            - echo
-            - "Hello, world"
-            image: alpine:latest
-            name: example-job
-            resources:
-              requests:
-                cpu: "1"
-    status: {}
-"""
+
+def _resource(filename: str):
+    return Path(__file__).parent.joinpath("resources", filename).read_text()
