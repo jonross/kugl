173fe236df260033e58255aec2fa5d169a080557
Jonathan Ross <jonross@alum.mit.edu>
2024-12-17 21:45:36 -0500

    Alias fixes (#82)
    * Aliases should extend command line
    
    * Additional alias and CLI tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -12,14 +12,58 @@ from kugel.impl.utils import KugelError
 
 def test_enforce_one_cache_option(test_home):
     with pytest.raises(KugelError, match="Cannot use both -c/--cache and -u/--update"):
-        main("-c -u foo".split(" "))
+        main(["-c", "-u", "select 1"])
 
 
 def test_enforce_one_namespace_option(test_home):
     with pytest.raises(KugelError, match="Cannot use both -a/--all-namespaces and -n/--namespace"):
-        main("-a -n x foo".split(" "))
+        main(["-a", "-n", "x", "select 1"])
 
 
-def test_no_such_resource(test_home):
+def test_no_such_table(test_home):
     with pytest.raises(sqlite3.OperationalError, match="no such table: foo"):
         main(["select * from foo"])
+
+
+def test_unknown_alias(test_home):
+    with pytest.raises(KugelError, match="No alias named 'foo'"):
+        main(["foo"])
+
+
+def test_alias_with_invalid_option(test_home, capsys):
+    test_home.joinpath("init.yaml").write_text("""
+        alias:
+          foo:
+          - --badoption
+          - "select 1"
+    """)
+    with pytest.raises(SystemExit):
+        main(["-a", "foo"])
+    assert "unrecognized arguments: --badoption" in capsys.readouterr().err
+
+
+def test_unknown_option(test_home, capsys):
+    with pytest.raises(SystemExit):
+        main(["--badoption", "select 1"])
+    assert "unrecognized arguments: --badoption" in capsys.readouterr().err
+
+
+def test_enforce_one_cache_option_via_alias(test_home, capsys):
+    test_home.joinpath("init.yaml").write_text("""
+        alias:
+          foo:
+          - -u
+          - "select 1"
+    """)
+    with pytest.raises(KugelError, match="Cannot use both -c/--cache and -u/--update"):
+        main(["-c", "foo"])
+
+
+def test_simple_alias(test_home, capsys):
+    test_home.joinpath("init.yaml").write_text("""
+        alias:
+          foo: ["select 1, 2"]
+    """)
+    main(["foo"])
+    out, _ = capsys.readouterr()
+    assert out == "  1    2\n" * 2
