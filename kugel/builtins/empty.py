c855b25e52211b3c30e065909452ef280d132214
Jonathan Ross <jonross@alum.mit.edu>
2024-12-22 09:20:07 -0500

    Forgot to commit test domains

---
+++ kugel/builtins/empty.py
@@ -0,0 +1,26 @@
+"""
+Use this for queries that don't reference any tables.
+
+NOTE: This is not a good example of how to write user-defined tables.
+FIXME: Remove references to non-API imports.
+FIXME: Don't use ArgumentParser in the API.
+"""
+
+from argparse import ArgumentParser
+
+from kugel.api import domain
+
+
+@domain("empty")
+class Empty:
+
+    def add_cli_options(self, ap: ArgumentParser):
+        # FIXME, artifact of assuming kubernetes
+        self.namespace = "default"
+        pass
+
+    def handle_cli_options(self, args):
+        pass
+
+    def get_objects(self, *_):
+        return {}
