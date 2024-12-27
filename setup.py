d79514eafd45b7e0e31565aa6748f4035f924957
Jonathan Ross <jonross@alum.mit.edu>
2024-12-26 22:03:49 -0500

    Extend dependency ranges

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ setup(
     packages=find_packages(),  # Automatically finds `your_package_name/`
     include_package_data=True,
     install_requires=[
-        "arrow>=1.1.1,<=1.3.0",
-        "funcy>=1.15,<=1.18",
-        "jmespath>=1.0.0,<=1.0.1",
+        "arrow>=1.0.1,<=1.3.0",
+        "funcy>=1.13,<=1.18",
+        "jmespath>=0.9.5,<=1.0.1",
         "pydantic>=2.0.2,<=2.9.2",
         "pyyaml>=5.3,<=6.0.2",
-        "tabulate>=0.8.8,<=0.9.0",
+        "tabulate>=0.8.7,<=0.9.0",
     ],
     entry_points={
         "console_scripts": [
